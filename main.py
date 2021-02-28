4da7e2e3618ed95ca1d15820fdf4276a46a2aa76
Thor Jarle Kinstad <tj@MacBook-Tj.local>
2021-02-28 13:38:41 +0700

    added more info to json outfile

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 import requests, bs4, pandas, time, json
 
 # Ask Name and Price
-def get_app_info(app_id):
-    url = f'https://apps.apple.com/no/app/id{app_id}'
+def get_app_info(apple_id):
+    url = f'https://apps.apple.com/no/app/id{apple_id}'
     r = requests.get(url)
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
     name_element = soup.find(name='h1', attrs={'class': lambda string: 'title' in string})
@@ -15,7 +15,7 @@ def get_app_info(app_id):
     return app_id, name, price
 
 #get json file 
-with open ("appArrayInput.json") as file:
+with open ("inputFile.json") as file:
     data = json.load(file)
 
 # Make list
@@ -23,14 +23,14 @@ lst = list()
 
 #Lopp through objects and put vaules in list
 for i in range (len(data["value"])):
-    lst.append(data['value'][i]['appId'])
+    lst.append(data['value'][i]['apple_id'])
     print (lst)
 
 # Main // Get result, (print), export
 if __name__ == '__main__':
-    results = [get_app_info(app_id) for app_id in lst]
-    df = pandas.DataFrame(results, columns=['app_id', 'name', 'price'])
+    results = [get_app_info(apple_id) for app_id in lst]
+    df = pandas.DataFrame(results, columns=['apple_id', 'name', 'price'])
     print(df)
-    df.to_json('appleAppPrices.json')
+    df.to_json('outFile.json')
         
     
